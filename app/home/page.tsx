a41c7f77a596976ef12b32ccf9070b25ea6db4d3
aeroblurr <harryplaysbloxy@gmail.com>
2025-08-14 13:00:42 +0200

    first commit

---
+++ app/home/page.tsx
@@ -0,0 +1,15 @@
+    import React from 'react';
+    import './app.css';
+
+    function App() {
+      return (
+        <div className="container">
+          <h1 className="main-heading">Travelling,<br/>reimagined.</h1>
+
+        </div>
+
+
+      );
+    }
+
+    export default App;
